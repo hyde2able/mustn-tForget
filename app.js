6050049c35f462fc488f40a48922eb72e99cb599
hyde2able <hyde31415@gmail.com>
2015-12-04 23:34:52 +0900

    milkcocoaと連携
    テロの歴史を投稿できるようにする。

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 
 var express = require('express')
-    ,app = express();
+,   async = require('async')
+,   app = express();
 
 
 //////////////////  MilkCocoa ////////////////
-// var MilkCocoa = require('milkcocoa');
-// var milkcocoa = new MilkCocoa('readih652j8r.mlkcca.com');
-// var ds = milkcocoa.dataStore("ranking");// dataStore作成
+var MilkCocoa = require('milkcocoa');
+var milkcocoa = new MilkCocoa('teaihrlwcgv.mlkcca.com');
+var country = milkcocoa.dataStore('country').history();
 
 /* appの設定 */
 app.set('port', process.env.PORT || 3000);
@@ -15,14 +16,51 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 
+
+/* ここで死亡者数やテロ発生件数でlevel1, 2, 3, 4, 5を相対比率で割り振る */
+var setColor = function(countries, total, callback) {
+    color = {};
+    async.each(countries, function(c, next) {
+        color[c.country_id] = {fillKey: 'level1'}
+        next();
+    }, function(err) {
+        callback(countries, color);
+    });
+};
+
+
 app.get('/', function(req, res) {
 
+    country.size(20);
+    country.limit(200);
+    var countries = [];
+    var total = 0;
 
-    res.render('index', {});
-
-
-	return;
+    country.on('data', function(data) {
+        console.log(data);
+        data.forEach(function(datum) {
+            var c = {
+                id: datum.id,
+                name: datum.value.name,
+                country_id: datum.value.country_id,
+                death: datum.value.death,
+                number: datum.value.number
+            };
+            total += c.death;
+            countries.push(c);
+        });
     });
+
+    country.on('end', function() {
+        console.log(countries);
+        setColor(countries, total, function(countries, color) {
+            res.render('index', {country: countries, color: color});
+        });
+    });
+
+    country.run();
+
+});
 
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -9,11 +9,125 @@ $(function() {
         });
     }, 3000);
 
-	var world = new Datamap({element: document.getElementById('world')});
-
-
     var e = function(str) {
         return escape(str);
     }
+
+    var milkcocoa = new MilkCocoa('teaihrlwcgv.mlkcca.com');
+
+
+    /* 地図をクリックした時に、その国のIDを取得する。それを元にその国IDのデータストアの中身を取得する。 */
+    var getHistory = function(country) {
+        var name = country.fillKey;
+        var ds = milkcocoa.dataStore(name).history();
+        console.log(name);
+    };  
+
+
+
+    var fillColor = {
+        'level1': '#F4CC48',
+        'level2': '#EDA648',
+        'level3': '#FF5E48',
+        'level4': '#874140',
+        'level5': '#FF0007',
+        defaultFill: '#EDDC4E'
+    };
+
+    var fillKey = {
+        'RUS': {fillKey: 'RUS'},
+        'PRK': {fillKey: 'PRK'},
+        'PRC': {fillKey: 'PRC'},
+        'IND': {fillKey: 'IND'},
+        'GBR': {fillKey: 'GBR'},
+        'FRA': {fillKey: 'FRA'},
+        'PAK': {fillKey: 'PAK'},
+        'USA': {fillKey: 'USA'}
+    };
+
+	var map = new Datamap({
+        element: document.getElementById('world'),
+        scope: 'world',
+        geographyConfig: {
+            hideAntarctica: true,   /* 北極は載せない */
+            boderWidth: 1,
+            borderColor: '#FFFFFF',
+            popupOnHover: true,
+            popupTemplate: function(geography, data) {
+                //console.log(geography);
+                return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong></div>';
+            },
+            highlightOnHover: true, /* ホバー時にエフェクト */
+            highlightFillColor: '#FC8D59',
+            highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+            highlightBorderWidth: 2
+        },
+        bubblesConfig: {
+            popupOnHover: true,
+            radius: null,
+            popupTemplate: function(geography, data) {
+                return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
+            },
+            clickAction: function(data) {
+                getHistory(data);
+            },
+            actionOnClick: true,
+        },
+        fills: fillColor,
+        data: fillKey
+    });
+
+
+    // var terro = [{
+    //     name: 'Joe 4',
+    //     radius: 25,
+    //     yield: 400,
+    //     country: 'USSR',
+    //     fillKey: 'RUS',
+    //     significance: 'First fusion weapon test by the USSR (not "staged")',
+    //     date: '1953-08-12',
+    //     latitude: 50.07,
+    //     longitude: 78.43
+    // },{
+    //     name: 'RDS-37',
+    //     radius: 40,
+    //     yield: 1600,
+    //     country: 'USSR',
+    //     fillKey: 'RUS',
+    //     significance: 'First "staged" thermonuclear weapon test by the USSR (deployable)',
+    //     date: '1955-11-22',
+    //     latitude: 50.07,
+    //     longitude: 78.43
+    // },{
+    //     name: 'Tsar Bomba',
+    //     radius: 75,
+    //     yield: 50000,
+    //     country: 'USSR',
+    //     fillKey: 'RUS',
+    //     significance: 'Largest thermonuclear weapon ever tested—scaled down from its initial 100 Mt design by 50%',
+    //     date: '1961-10-31',
+    //     latitude: 73.482,
+    //     longitude: 54.5854
+    // }
+    // ];
+
+    var hoverinfo = function(geo, data) {
+        var hover = '<div class="hoverinfo">' + data.name;
+        hover += '<br/>Payload: ' + data.yield + ' kilotons';
+        hover += '<br/>Country: ' + data.country + '';
+        hover += '<br/>Date: ' + data.date + '';
+        hover += '</div>';
+        return hover;
+    };
+
+    // map.bubbles(terro, {
+    //     popupTemplate: function(geo, data) {
+    //         return hoverinfo(geo, data);
+    //     },
+    // });
+
+
+
+
 
 });
